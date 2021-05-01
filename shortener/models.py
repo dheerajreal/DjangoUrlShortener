e27edd06ca3904a4d106c7503d051e2e1d7645ba
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-05-02 01:39:54 +0530

    add model for urlrecord

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class UrlRecord(models.Model):
+    short_url = models.CharField(
+        primary_key=True,
+        editable=False,
+        max_length=settings.URLTAG_LENGTH
+    )
+    original_url = models.URLField(blank=False, null=False)
+    user = models.ForeignKey(
+        User,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
+    date_created = models.DateField(auto_now_add=True)
+    date_expiry = models.DateField()
+
+    def __str__(self):
+        return self.original_url
