532f611e9f6c121fd47a9ce7dbdfd49217e28a8c
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-05-02 21:09:34 +0530

    add urlrecord on admin panel

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,5 +1,7 @@
-from django.contrib import admin
 from django.conf import settings
+from django.contrib import admin
+
+from .models import UrlRecord
 
 site_title = settings.ADMIN_PANEL_WEBSITE_TITLE
 # change admin interface
@@ -8,4 +10,21 @@ admin.site.site_header = site_title
 admin.site.site_title = site_title
 admin.site.site_url = "/"
 
-# Register your models here.
+
+class UrlRecordAdmin(admin.ModelAdmin):
+
+    # things to go on admin page
+    list_display = [
+        "original_url",
+        "short_url",
+        "date_created",
+        "date_expiry",
+        "user"
+    ]
+    # can filter by date_created and date_expiry
+    list_filter = ["date_created", "date_expiry"]
+    #  allow search
+    search_fields = ["original_url", "short_url"]
+
+
+admin.site.register(UrlRecord, UrlRecordAdmin)
